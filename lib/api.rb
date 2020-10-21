e404f39213fa0dec8021a449417e795fabb23a9b
Alan Young <alanryoung01@gmail.com>
2020-10-21 17:30:41 +0000

    Expanded upon the API and run/executable

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,12 @@
 class API
 
+  def self.poke
+    url = "https://pokeapi.co/api/v2/"
+    binding.pry
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    hash = JSON.parse(response)
+
+  end
+
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,7 @@
 class CLI
+  def start
+    puts "Welcome to the Pokemon CLI!"
+    API.poke
+  end
 
 end
